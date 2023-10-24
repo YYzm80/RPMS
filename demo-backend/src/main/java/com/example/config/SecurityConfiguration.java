71e15e2309cde4fc00a472813d77ddba69ea7684
User <3107907150@qq.com>
2023-10-24 14:01:47 +0800

    后端：SpringSecurity基本配置

---
+++ demo-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.example.config;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.example.entity.RestBean;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(conf -> {
+                    conf.anyRequest().authenticated();
+                })
+                .formLogin(conf -> {
+                    conf.loginProcessingUrl("/api/auth/login");
+                    conf.successHandler(this::onAuthenticationSuccess);
+                    conf.failureHandler(this::onAuthenticationFailure);
+                })
+                .logout(conf -> {
+                    conf.logoutUrl("/api/auth/logout");
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(conf -> {
+                    conf.authenticationEntryPoint(this::onAuthenticationFailure);
+                })
+                .build();
+    }
+
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.success("登录成功！")));
+    }
+
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401, exception.getMessage())));
+    }
+}
