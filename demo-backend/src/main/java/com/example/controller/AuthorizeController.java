1755243e909da9e0a79d06ab8c607213d6b7db3d
User <3107907150@qq.com>
2023-10-28 15:25:03 +0800

    后端：用户注册实现

--- demo-backend/src/main/java/com/example/controller/AuthorizeController.java
+++ demo-backend/src/main/java/com/example/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.example.service.AuthorizeService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthorizeController {
 
+    private final String REGEXP = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$";
+    private final String USERNAME_REGEXP = "^[a-zA-Z0-9一-龥]+$";
+
     @Resource
     AuthorizeService service;
 
     @PostMapping("/valid-email")
-    public RestBean<String> validateEmail(@Pattern (regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$")
-                                              @RequestParam("email") String email,
+    public RestBean<String> validateEmail(@Pattern(regexp = REGEXP)
+                                          @RequestParam("email") String email,
                                           HttpSession session) {
-        if (service.sendValidateEmail(email, session.getId()))
+        String s = service.sendValidateEmail(email, session.getId());
+        if (s == null)
             return RestBean.success("邮件发送成功，请注意查收");
         else
-            return RestBean.failure(400, "邮件发送失败，请联系管理员");
+            return RestBean.failure(400, s);
+    }
+
+    @PostMapping("/register")
+    public RestBean<String> registerUser(@Pattern(regexp = USERNAME_REGEXP) @Length(min = 2, max = 10) @RequestParam("username") String username,
+                                         @Length(min = 6, max = 16) @RequestParam("password") String password,
+                                         @Pattern(regexp = REGEXP) @RequestParam("email") String email,
+                                         @Length(min = 6,max = 6) @RequestParam("code") String code,
+                                         HttpSession session) {
+        String s = service.validateAndRegister(username, password, email, code, session.getId());
+        if (s == null){
+            return RestBean.success("注册成功");
+        } else {
+            return RestBean.failure(400, s);
+        }
     }
 }
--- demo-frontend/src/components/welcome/RegisterPage.vue
+++ demo-frontend/src/components/welcome/RegisterPage.vue
@@ -74,6 +74,7 @@ const rules = {
 
 const isEmailValid = ref(false)
 const formRef = ref()
+const coldTime = ref(0)
 
 const onValidate = (prop, isValid) => {
   if (prop === 'email') {
@@ -84,7 +85,15 @@ const onValidate = (prop, isValid) => {
 const register = () => {
   formRef.value.validate((isValid) => {
     if (isValid) {
-
+        post('/api/auth/register', {
+            username: form.username,
+            password: form.password,
+            email: form.email,
+            code: form.code
+        }, (message) => {
+            ElMessage.success(message)
+            router.push('/')
+        })
     } else {
       ElMessage.warning('请完整填写注册信息！')
     }
@@ -96,6 +105,8 @@ const validateEmail = () => {
     email: form.email
   }, (message) => {
     ElMessage.success(message)
+      coldTime.value = 60
+      setInterval(() => coldTime.value--, 1000)
   })
 }
 </script>
@@ -111,7 +122,7 @@ const validateEmail = () => {
     <div style="margin-top: 50px">
       <el-form :model="form" :rules="rules" @validate="onValidate" ref="formRef">
         <el-form-item prop="username">
-          <el-input v-model="form.username" type="text" placeholder="用户名">
+          <el-input v-model="form.username" type="text" :maxlength="10" placeholder="用户名">
             <template #prefix>
               <el-icon>
                 <User/>
@@ -120,7 +131,7 @@ const validateEmail = () => {
           </el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="form.password" type="password" placeholder="密码">
+          <el-input v-model="form.password" type="password" :maxlength="16" placeholder="密码">
             <template #prefix>
               <el-icon>
                 <Lock/>
@@ -129,7 +140,7 @@ const validateEmail = () => {
           </el-input>
         </el-form-item>
         <el-form-item prop="password_repeat">
-          <el-input v-model="form.password_repeat" type="password" placeholder="重复密码">
+          <el-input v-model="form.password_repeat" type="password" :maxlength="16" placeholder="重复密码">
             <template #prefix>
               <el-icon>
                 <Lock/>
@@ -149,7 +160,7 @@ const validateEmail = () => {
         <el-form-item prop="code">
           <el-row :gutter="10" style="width: 100%">
             <el-col :span="17">
-              <el-input v-model="form.code" type="text" placeholder="请输入验证码">
+              <el-input v-model="form.code" type="text" :maxlength="6" placeholder="请输入验证码">
                 <template #prefix>
                   <el-icon>
                     <EditPen/>
@@ -158,7 +169,8 @@ const validateEmail = () => {
               </el-input>
             </el-col>
             <el-col :span="6">
-              <el-button @click="validateEmail" type="success" :disabled="!isEmailValid">获取验证码</el-button>
+              <el-button @click="validateEmail" type="success" :disabled="!isEmailValid || coldTime > 0">
+                  {{coldTime > 0 ? coldTime + '秒后可发送' : '获取验证码' }}</el-button>
             </el-col>
           </el-row>
         </el-form-item>
