ee415530cd85068e0c85d70f5da796da5e825917
Yz820 <3107907150@qq.com>
2025-05-22 16:56:52 +0800

    refactor: 更新项目路径和名称
    - 将 DPMS 相关路径更改为 RPMS

--- demo-backend/src/main/java/com/example/util/consts/Const.java
+++ demo-backend/src/main/java/com/example/util/consts/Const.java
@@ -5,7 +5,7 @@ public class Const {
     public static final String PAYMENT_SESSION = "payment:session:";
     public static final String LOCATION_API = "https://restapi.amap.com/v3/ip?key=077e7585e935dd09dcdf2098c801cad4";
     public static final String WEATHER_API = "https://restapi.amap.com/v3/weather/weatherInfo?key=077e7585e935dd09dcdf2098c801cad4";
-    public static String UPLOAD_PATH = "/home/DPMS/uploaded/";
+    public static String UPLOAD_PATH = "/home/RPMS/uploaded/";
     public static String FRONT_PATH = "http://8.155.59.231/";
 
     // 角色
